a4df1788b8558828ad9895a2b10af3b9d7357bc5
Juan Felipe Rada <radapls8@gmail.com>
2023-03-10 14:23:43 -0300

    feat: Create base project and add packages

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * RADAPLS PROJECTS
+ * ------------------
+ * Copyright (C) 2023 Juan Felipe Rada - All Rights Reserved.
+ *
+ * This file, project or its parts can not be copied and/or distributed without
+ * the express permission of Juan Felipe Rada.
+ *
+ * @file App.tsx
+ * @author Juan Felipe Rada <radapls8@gmail.com>
+ * @date Friday, 10th March 2023
+ */
+
+export default function App()
+{
+    return (
+        <h1>
+            App
+        </h1>
+    )
+}
